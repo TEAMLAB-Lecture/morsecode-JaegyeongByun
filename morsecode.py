13872ae88dd89742fd365ba748a9e1e302f74d95
JaegyeongByun <worud1208@gmail.com>
2021-01-21 18:56:28 +0900

    Update my code

--- morsecode.py
+++ morsecode.py
@@ -94,7 +94,7 @@ def is_validated_english_sentence(user_input):
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
     no_space_user_input = ''.join(user_input.split(" "))
-    p = re.compile(r"[.,!?]*[a-zA-Z]+[a-zA-Z.,!?]*")
+    p = re.compile(r'[.,!?]*[a-zA-Z]+[a-zA-Z.,!?]*')
     if no_space_user_input == '':
         result = False
     elif p.fullmatch(no_space_user_input):
